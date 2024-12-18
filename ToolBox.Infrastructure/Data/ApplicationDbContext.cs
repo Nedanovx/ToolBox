2db5a6a9b5abe91debbbc1a6cd917ce132388c11
Nikola Nedanov <nedanovx@gmail.com>
2024-12-18 13:12:19 +0200

    added first migration

--- ToolBox.Infrastructure/Data/ApplicationDbContext.cs
+++ ToolBox.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
+using ToolBox.Infrastructure.Models;
 
 namespace ToolBox.Infrastructure.Data
 {
@@ -9,5 +12,25 @@ namespace ToolBox.Infrastructure.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.Entity<ProductSubCategory>()
+        .HasKey(ps => new { ps.ProductId, ps.SubCategoryId });
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<SubCategory> SubCategories { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Comment> Comments { get; set; } = null!;
+        public DbSet<ProductSubCategory> ProductSubCategories { get; set; } = null!;
+        public DbSet<DeliveryAddress> DeliveryAddresses { get; set; } = null!;
+        public DbSet<Cart> Carts { get; set; } = null!;
+        public DbSet<CartItem> CartItems { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<OrderItem> OrdersItem { get; set; } = null!;
     }
 }
