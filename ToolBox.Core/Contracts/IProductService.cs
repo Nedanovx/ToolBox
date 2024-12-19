c2c3b6ca324bcca98d09a8929b3da60d53538b6c
Nikola Nedanov <nedanovx@gmail.com>
2024-12-19 20:47:35 +0200

    Added Product service, interface

---
+++ ToolBox.Core/Contracts/IProductService.cs
@@ -0,0 +1,8 @@
+﻿using ToolBox.Core.Models.Product;
+namespace ToolBox.Core.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductQueryModel>> AllProductAsync();
+    }
+}
---
+++ ToolBox.Core/Services/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using ToolBox.Infrastructure.Common;
+using ToolBox.Infrastructure.Models;
+using ToolBox.Core.Models.Product;
+using ToolBox.Core.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToolBox.Core.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IRepository repository;
+
+        public ProductService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<IEnumerable<ProductQueryModel>> AllProductAsync()
+        {
+            return await repository.AllAsync<Product>()
+                .Where(p => p.IsVisible == true)
+                .Select(p => new ProductQueryModel()
+                {
+                    Id = p.Id,
+                    ProductName = p.ProductName,
+                    SKU = p.SKU,
+                    ProductDescription = p.ProductDescription,
+                    Size = p.Size,
+                    Characteristic = p.Characteristic,
+                    ImageBase64 = p.Image != null
+                    ? Convert.ToBase64String(p.Image)
+                    : string.Empty,
+                    ProductPrice = p.ProductPrice,
+                    Quantity = p.Quantity,
+                    Weight = p.Weight,
+                    IsInStock = p.IsInStock,
+                    IsPromo = p.IsPromo,
+                    PromoPrice = p.PromoPrice,
+                })
+                .OrderByDescending(p => p.Id)
+                .ToListAsync();
+        }
+    }
+}
