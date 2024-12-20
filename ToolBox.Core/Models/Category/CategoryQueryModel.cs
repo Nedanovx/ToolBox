4363c0c889ad5e6d250c54ee9d03b5acd4c78169
Nikola Nedanov <nedanovx@gmail.com>
2024-12-20 19:54:30 +0200

    added CategoryQuery and SubCategoryQuery model

---
+++ ToolBox.Core/Models/Category/CategoryQueryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ToolBox.Core.Models.Category
+{
+    public class CategoryQueryModel
+    {
+        public string CategoryName { get; set; } = string.Empty;
+
+        public ICollection<SubCategoryQueryModel> SubCategories { get; set; } = new HashSet<SubCategoryQueryModel>();
+    }
+}
