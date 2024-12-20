3cd0df0f9e2a9bb30875450d36552caf3027a00f
Nikola Nedanov <nedanovx@gmail.com>
2024-12-20 19:54:57 +0200

    added main menu component

---
+++ ToolBox/Components/MainMenuComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToolBox.Core.Contracts;
+
+namespace ToolBox.Components
+{
+    public class MainMenuComponent : ViewComponent
+    {
+        private readonly ICategoryService categoryService;
+
+        public MainMenuComponent(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await categoryService.AllCategoryAsync();
+
+            return await Task.FromResult(View(categories));
+        }
+    }
+}
