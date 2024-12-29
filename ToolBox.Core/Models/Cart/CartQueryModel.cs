d9479455a1e09d4fc06aeb6f5ff38b8601fdd3c6
Nikola Nedanov <nedanovx@gmail.com>
2024-12-29 19:03:02 +0200

    cart query model

---
+++ ToolBox.Core/Models/Cart/CartQueryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBox.Core.Models.Cart
+{
+    public class CartQueryModel
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+
+        public ICollection<CartItemsQueryModel> CartItems { get; set; } = new HashSet<CartItemsQueryModel>();
+    }
+}
