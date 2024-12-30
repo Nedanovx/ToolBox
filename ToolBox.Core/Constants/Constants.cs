065302ecc9d0791bc1e75e60bfe80953f70a78a7
Nikola Nedanov <nedanovx@gmail.com>
2024-12-30 17:05:52 +0200

    added constants min first and last name

--- ToolBox.Core/Constants/Constants.cs
+++ ToolBox.Core/Constants/Constants.cs
@@ -11,5 +11,12 @@
         public const string RequiredErrorMessage = "The {0} field is required";
 
         public const string CommentLengthErrorMessage = "The {0} field must be between {2} and {1} characters long !";
+
+        /// <summary>
+        /// Min User's first name length
+        /// </summary>
+        public const int MinAppUserFirstNameLength = 3;
+
+        public const int MinAppUserLastNameLength = 4;
     }
 }
